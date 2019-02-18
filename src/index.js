71aaf2bed0a039adc7599b624ccd334a42b8acea
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-02-18 10:03:57 +1000

    refactor(plugin): change node requires to esmodule imports

--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@
  * Created by zhy on 2017/4/1.
  */
 
+ import steemEditor from './steem-editor.vue';
+ import LeftToolbar from './components/md-toolbar-left.vue';
+ import RightToolbar from './components/md-toolbar-right.vue';
 
 /**
  * steemEditor
  * @author hinesboy
  */
-const steemEditor = require( './steem-editor.vue' );
 function renderMarkdown( value ) {
   if ( !value ) {
     return '';
@@ -17,8 +19,8 @@ function renderMarkdown( value ) {
 
 const VuesteemEditor = {
   steemEditor,
-  LeftToolbar: require( './components/md-toolbar-left' ),
-  RightToolbar: require( './components/md-toolbar-right' ),
+  LeftToolbar,
+  RightToolbar,
   install( Vue ) {
     Vue.component( 'steem-editor', steemEditor );
     Vue.filter( 'renderMD', renderMarkdown );
@@ -26,4 +28,4 @@ const VuesteemEditor = {
   },
 };
 
-module.exports = VuesteemEditor;
+export default VuesteemEditor;
