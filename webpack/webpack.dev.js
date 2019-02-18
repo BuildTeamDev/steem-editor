23c1fc9cebde4e59b08fc82508fe05b5af95f516
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-02-18 10:01:50 +1000

    chore(webpack): remove commons chunk plugin from dev example

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -5,6 +5,7 @@ const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
+    mode: 'development',
     entry: {
         index: './src/dev/index.js',
         vue: ['vue']
@@ -32,11 +33,6 @@ const config = {
 
 const res = merge([base, config]);
 res.plugins = [
-    new webpack.optimize.CommonsChunkPlugin({
-        names: ['vue', 'common'],
-        filename: 'js/[name].[chunkhash:8].js',
-        minChunks: Infinity
-    }),
     new HtmlWebpackPlugin({
         filename: 'index.html',
         template: 'src/dev/index.html',
