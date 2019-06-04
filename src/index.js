e075c2c099a824c757443ae0bf97d643a451a476
ReggaeMuffin <644950+reggaemuffin@users.noreply.github.com>
2019-06-04 14:14:32 +0200

    adding steempeak/condenser like alternative parsing

--- src/index.js
+++ src/index.js
@@ -2,28 +2,32 @@
  * Created by zhy on 2017/4/1.
  */
 
- import steemEditor from './steem-editor.vue';
- import LeftToolbar from './components/md-toolbar-left.vue';
- import RightToolbar from './components/md-toolbar-right.vue';
+import LeftToolbar from "./components/md-toolbar-left.vue";
+import RightToolbar from "./components/md-toolbar-right.vue";
+import { parseHtml } from "./lib/mixins/parser.js";
+import steemEditor from "./steem-editor.vue";
 
 /**
  * steemEditor
  * @author hinesboy
  */
-function renderMarkdown( value ) {
+function renderMarkdown(value) {
   if ( !value ) {
-    return '';
+    return "";
   }
-  return steemEditor.mixins[0].data().markdownIt.render( value );
+  return parseHtml(value);
+  return steemEditor.mixins[ 0 ].data()
+    .markdownIt
+    .render(value);
 }
 
 const VuesteemEditor = {
   steemEditor,
   LeftToolbar,
   RightToolbar,
-  install( Vue ) {
-    Vue.component( 'steem-editor', steemEditor );
-    Vue.filter( 'renderMD', renderMarkdown );
+  install(Vue) {
+    Vue.component("steem-editor", steemEditor);
+    Vue.filter("renderMD", renderMarkdown);
     Vue.prototype.$renderMD = renderMarkdown;
   },
 };
